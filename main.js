c7bc8b3ac3469b10862fc0d3036e705fd8d22183
Odisseuss <rares_lungescu@yahoo.com>
2020-01-31 10:11:46 +0000

    WIP add stealth mode shortcuts feature

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron');
+const { app, BrowserWindow, globalShortcut } = require("electron");
 //const yeeter = require('./yeeter.js');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
-
-
-
 
 function createWindow() {
   // Create the browser window.
@@ -22,16 +16,18 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  win.loadFile('index.html');
+  win.loadFile("index.html");
 
   // Open the DevTools.
   //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("close", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+    // Unregiste all shortcuts when app is closed
+    globalShortcut.unregisterAll();
     win = null;
   });
 }
@@ -39,18 +35,33 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", () => {
+  // Create shortcuts for stealth mode on app ready
+  const hideWindowShortcut = globalShortcut.register(
+    "CommandOrControl+Shift+P",
+    () => {
+      win.hide();
+    }
+  );
+  const showWindowShortcut = globalShortcut.register(
+    "CommandOrControl+P",
+    () => {
+      win.show();
+    }
+  );
+  createWindow();
+});
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
